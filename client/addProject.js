57b5f0627ab1f220789ea47eca5fd8842f20926d
theresaanna <theresa@theresaanna.com>
2012-05-06 00:34:26 -0400

    restructure data, build out proper templating, start of add form

--- client/addProject.js
+++ client/addProject.js
@@ -1,7 +1,15 @@
 Template.addProjectLink.events = {
     'click #addProjectSubmit': function(e) {
-//        Project.insert();
-        console.log('hey');
+        e.preventDefault();
+        
+        var name = $('[name="projectName"]').attr('value'),
+            formMilestones = $('[name="milestone"]').attr('value'),
+            formTasks = $('[name="task"]').attr('value');    
+        
+        $(formTasks).each(function() {
+            console.log(this);
+        });
+
     },
 
     'click .expand': function(e) {
--- goalie.js
+++ goalie.js
@@ -1,17 +1,28 @@
 Project = new Meteor.Collection('projects');
+Milestone = new Meteor.Collection('milestones');
+Task = new Meteor.Collection('tasks');
 
 if (Meteor.is_client) {
   Template.projects.project = function () {
     return Project.find();
   };
 
-  Template.projects.milestones = function() {
-    return Project.find('milestones.milestone');
+  Template.project.milestones = function() {
+    var milestones = this.milestones;
+    return Milestone.find({_id: {$in: milestones}});
   };
 
-  Template.projects.milestones.tasks = function() {
-    return Project.find('milestones.tasks');
+  Template.milestones.tasks = function() {
+    // minimongo dot notation doesn't seem to be working on 0.3.5 either
+    // I *should* be able to do this.tasks.t_id if I store ids in objects with t_id 
+    // as a key. works in the mongo console but not here
+    that = this.tasks;
+    return Task.find({_id: {$in: that}});
   };
+
+  Template.tasks = function() {
+    return Task.find();
+  }
 
   // Template.projects.events = {
     //'click .edit' : function () {
@@ -28,4 +39,9 @@ if (Meteor.is_server) {
   });
 }
 
-// Project.insert({projectName: "Do laundry", status: "in progress", milestones: [ {milestone: 'get laundry together', tasks: [ { task: 'find laundry', status: 0}, { task: 'get laundry card', status: 0}, { task: 'get detergent', status: 0} ]}, {milestone: 'fold laundry'}]});
+// Milestone.update({_id: "1fb92586-7fb2-4a19-8ca9-ed145e74070d"}, {name: "get laundry together", tasks: ["d72d2bc4-b717-4111-bc4f-ef72eaa49e10", "4686be94-0e54-4a43-bba8-1463f49eae80", "56c74fa2-274d-406e-813e-9718d64bfab9", "73b32867-3809-47fe-94fe-893de963f076"]});
+// Project.insert({name: "Do laundry", milestones: ["1fb92586-7fb2-4a19-8ca9-ed145e74070d", "6143e13d-2e5a-4993-8eae-d50149fba26f"]});
+// Milestone.update({name: "fold laundry"}, {$addToSet: {tasks: ["56c74fa2-274d-406e-813e-9718d64bfab9"]}});
+// Milestone.insert({name: "fold laundry", tasks: ["56c74fa2-274d-406e-813e-9718d64bfab9"]});
+// Project.insert({name: "Do laundry", status: "in progress", milestones: ["1fb92586-7fb2-4a19-8ca9-ed145e74070d", "6143e13d-2e5a-4993-8eae-d50149fba26f"]});
+//Project.update({name: "Do laundry"}, {$addToSet: {milestones: "61bb61b9-8af5-42a3-b954-155211bee8b9"}});
