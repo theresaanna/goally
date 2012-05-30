e47f58500e2fbfa9b9b6dfad741a1871986aad5e
theresaanna <theresa@theresaanna.com>
2012-05-30 02:43:08 -0400

    status changing

--- client/dashboard/addProject.js
+++ client/dashboard/addProject.js
@@ -2,7 +2,7 @@ Template.addProject.events = {
     'click #addProjectSubmit': function(e) {
         e.preventDefault();
         var projName = document.getElementById('newProjectName').value;
-        Project.insert({ name: projName, status: 'inprogress', created: Date.now() });
+        Project.insert({ name: projName, status: 'In Progress', created: Date.now() });
         $('.expandable').toggle();
     }
 };
--- client/dashboard/dashboard.js
+++ client/dashboard/dashboard.js
@@ -1,147 +1,24 @@
-Template.projects.project = function () {
-    return Project.find({uid: Session.get('user')}, {sort: {created: -1}});
-};
-
-Template.project.date = function() {
-    return dateFormat(this.created, "mmmm dd 'yy");
-};
-
-Template.project.milestones = function() {
-    var milestones = this.milestones;
-    if (typeof milestones !== 'undefined') {
-            return Milestone.find({_id: {$in: milestones}}, {sort: {created: 1}}); 
-        }
-    else {
-        return null;
-    }
-};
-
-// when implementing sort 
-// insert an 'order' index in db if a project's milestones or a milestone's tasks have ever been sorted
-// sort by 'order' if exists
-// activate sort -> fill in 'order' according to natural sort
-// 
-
-Template.milestones.tasks = function() {
-    // minimongo dot notation doesn't seem to be working on 0.3.5 either
-    // I *should* be able to do this.tasks.t_id if I store ids in objects with t_id 
-    // as a key. works in the mongo console but not here
-    that = this.tasks;
-    if (typeof that !== 'undefined') {
-        return Task.find({_id: {$in: that}});
-    }
-    else {
-        return null;
-    }
-};
-
-Template.tasks = function() {
-    return Task.find();
+Template.itemStatus.list = function() {
+    return Status.find();
 }
-
-Template.addTaskNotesForm = function() {
-    return Task.find();
-}
-
-Template.projects.addMilestoneForm = function() {
-    // adds related project for input element data attribute
-    return Project.find();
-}
-
-Template.milestones.addTaskForm = function() {
-    // adds related milestone for input element data attribute
-    return Milestone.find();
-}
-
-Template.editStatusForm = function() {
-    return Milestone.find();
-}
-
-Template.editStatusForm.statuses = ['In Progress', 'Todo', 'Done'];
-
-// event handlers
-Template.project.events = {
-    'click .addMilestoneLink': function(event) {
-        event.preventDefault();
-        $(event.target).next('.addMilestoneForm').toggle();
-    },
-
-    'click .addToArchiveLink': function(event) {
-        event.preventDefault();
-        var projectName = $(event.target).attr('data-project-name'),
-            project = Project.findOne({name: projectName});
-        ProjectArchive.insert({name: project.name, milestones: project.milestones, status: project.status});
-        Project.remove({name: projectName});
-    }
-};
-
-Template.addMilestoneForm.events = {
-    'click #addMilestoneSubmit': function(event) {
-        event.preventDefault();
-        var field = $(event.target).prev('.milestoneName'),
-            fieldValue = $(field).val(),
-            projectName = $(field).attr('data-project-name');
-        var milestoneId = Milestone.insert({name: fieldValue, 'status': "pending", created: Date.now()});
-        Project.update({name: projectName}, {$addToSet: {milestones: milestoneId}});
-    }
-};
-
-Template.milestones.events = {
-    'click .addTaskLink': function(event) {
-        event.preventDefault();
-        $(event.target).next('.addTaskForm').toggle();
-    }
-};
-
-Template.addTaskForm.events = {
-    'click #addTaskSubmit': function(event) {
-        event.preventDefault();
-        var field = $(event.target).prev('.taskName'),
-            fieldVal = $(field).val(),
-            milestoneName = $(field).attr('data-milestone-name');
-        var taskId = Task.insert({name: fieldVal, 'status': "pending", created: Date.now()});
-        Milestone.update({name: milestoneName}, {$addToSet: {tasks: taskId}});
-    }
-};
-
-Template.tasks.events = {
-    'click .addTaskNotesLink': function(event) {
-        event.preventDefault();
-        $(event.target).next('.addTaskNotesForm').toggle();
-    },
-
-    'click .removeTask': function(event) {
-        event.preventDefault();
-        var taskName = $(event.target).prev().html();   
-        Task.remove({name: taskName});
-   }
-};
-
-Template.addTaskNotesForm.events = {
-    'click #addTaskNotesSubmit': function(event) {
-        event.preventDefault();
-        var field = $(event.target).prev('.addTaskNotes'),
-            fieldVal = $(field).val(),
-            taskName = $(field).attr('data-task-name');
-        Task.update({name: taskName}, {$addToSet: {notes: fieldVal}});
-    }
-};
 
 Template.itemStatus.events = {
     'click .changeStatus': function(event) {
         event.preventDefault();
-        $(event.target).next('.editStatusForm').toggle();
+        $(event.target).next('.statusList').toggle();
+    },
+
+    'click .statusListItem': function(event) {
+        var newStatus = $(event.target).html();
     }
 };
 
-Template.editStatusForm.events = {
-    'select .status': function(event) {
-        event.preventDefault();
-        return 'hi';
-        // figure out diff statuses for item scope
-        // find way to grab item scope - probably off data attr
-        // populate radio buttons with statuses
-            // current status selected
-        // run update
+// compare the Status obj and the Project.status
+// if true, the template will have the radio button
+// selected by default
+Handlebars.registerHelper("ifCurrentStatus", function(status, name) {
+    if (status === name) {
+        return 'checked';
     }
-};
+   return null;
+});
